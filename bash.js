e8ed1ee4442af0903eb07b2ee76782f3b3c36ce9
Burcu Noyan <burcunoyan@gmail.com>
2017-07-14 20:26:01 -0400

    add pwd and date commands

--- bash.js
+++ bash.js
@@ -6,5 +6,15 @@ process.stdin.on('data', function(data) {
   var cmd = data.toString().trim(); // remove the newline
 
   process.stdout.write('You typed: ' + cmd);
+
+  if (cmd === 'pwd') {
+    process.stdout.write('\n' + process.cwd());
+  }
+
+  if (cmd === 'date') {
+    process.stdout.write('\n' + Date().toString());
+  }
+
   process.stdout.write('\nprompt > ');
 });
+
