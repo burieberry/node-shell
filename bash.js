40bc83d861f765c49ce59a6ae9c2b00a7b2643dc
Burcu Noyan <burcunoyan@gmail.com>
2017-07-14 21:35:52 -0400

    refactor function call

--- bash.js
+++ bash.js
@@ -6,10 +6,7 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
   var cmd = data.toString().trim().split(' '); // remove the newline
-
-  if (cmd[0] === 'pwd') commands.pwd();
-  if (cmd[0] === 'date') commands.date();
-  if (cmd[0] === 'ls') commands.ls();
-  if (cmd[0] === 'echo') commands.echo(cmd.slice(1));
+  var fn = cmd[0],
+    file = cmd.slice(1);
+  commands[fn](file);
 });
-
